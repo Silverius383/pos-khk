7c7e8c777295b4908598c03dc7f546357efdceba
Silverius383 <stanicsetyaputra30@gmail.com>
2026-03-01 02:00:19 +0700

    fix pwa 4

--- proxy.ts
+++ proxy.ts
@@ -35,5 +35,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|_next/webpack-hmr).*)"],
+  matcher: ["/((?!_next|.*\\..*).*)"],
 };
